528bde2aa4a71e8a84a636150277aef9f0a87058
iromonek39 <kentarou.omori0309@gmail.com>
2023-03-20 15:20:09 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) :
+				?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+			endwhile;
+
+			the_posts_navigation();
+		endif;
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
