b748ad7539821064ee588d1a43202cdde01e3c9c
iromonek39 <kentarou.omori0309@gmail.com>
2023-03-20 15:28:57 +0900

    test

--- index.php
+++ index.php
@@ -1,29 +1,11 @@
-<?php
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-			endwhile;
-
-			the_posts_navigation();
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<div id="content">
+<?php if(have_posts()): while (have_posts()):the_post(); ?>
+<div class="post">
+　　<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<!-- 　　<?php the_excerpt(); ?> -->
+　　<p class="postinfo">
+　　<?php echo get_the_date(); ?><?php the_time(); ?>｜カテゴリー：<?php the_category(' , '); ?>
+　　</p>       
+</div>
+<?php endwhile; endif; ?>
+<div>
